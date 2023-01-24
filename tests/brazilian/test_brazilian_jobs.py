a5ff93551dd8bd2f440a35edb3d9a5cdddf83723
alissonRH <alisson.honorato@live.com>
2023-01-24 17:43:16 -0300

    testes

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translated_jobs = {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+
+    assert (
+        read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0]
+        == translated_jobs
+    )
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,29 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_mock = [
+        {
+            "min_salary": "500",
+            "max_salary": "1600",
+            "date_posted": "2023-01-20",
+        },
+        {
+            "min_salary": "200",
+            "max_salary": "900",
+            "date_posted": "2023-02-24",
+        },
+        {
+            "min_salary": "300",
+            "max_salary": "1000",
+            "date_posted": "2023-01-10",
+        },
+    ]
+    descending_order_jobs = [jobs_mock[0], jobs_mock[2], jobs_mock[1]]
+    ascending_order_jobs = [jobs_mock[1], jobs_mock[2], jobs_mock[0]]
+
+    sort_by(jobs_mock, "max_salary")
+    assert jobs_mock == descending_order_jobs
+
+    sort_by(jobs_mock, "min_salary")
+    assert jobs_mock == ascending_order_jobs
